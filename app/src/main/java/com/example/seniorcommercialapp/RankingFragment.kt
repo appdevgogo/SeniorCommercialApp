3075f7eb87fd78809c33f7a63030d7a27d80127d
Kyle Yoo <yeongjin.yoo.dev@gmail.com>
2022-10-24 00:00:05 +0900

    setting click event of drawer items

--- app/src/main/java/com/example/seniorcommercialapp/RankingFragment.kt
+++ app/src/main/java/com/example/seniorcommercialapp/RankingFragment.kt
@@ -3,18 +3,25 @@ package com.example.seniorcommercialapp
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
 
 class RankingFragment : Fragment() {
 
     private lateinit var toolbar: Toolbar
+    private lateinit var toolbar_title: TextView
+
     private lateinit var drawerLayout: DrawerLayout
+    private lateinit var drawerNavigationView: NavigationView
 
     private lateinit var adapter: RankingAdapter
     private lateinit var recyclerView: RecyclerView
@@ -28,13 +35,27 @@ class RankingFragment : Fragment() {
 
         //toolbar 세팅
         toolbar = view.findViewById(R.id.toolbar)
+        toolbar_title = toolbar.findViewById(R.id.toolbar_title)
+        toolbar_title.text = "롯데홈쇼핑"
         toolbar.setNavigationIcon(R.drawable.ic_drawer_normal) //toolbar의 왼쪽 메뉴 아이콘 버튼
         toolbar.setNavigationOnClickListener {
             drawerLayout.openDrawer(GravityCompat.START)
         }
 
+
         //drawer 세팅
         drawerLayout = view.findViewById(R.id.ranking_drawer_layout)
+        drawerNavigationView = drawerLayout.findViewById(R.id.ranking_drawer_nav_view)
+        drawerNavigationView.setNavigationItemSelectedListener {
+
+            when(it.itemId) {
+                R.id.menu_drawer_item1 -> reloadFragment("롯데홈쇼핑")
+                R.id.menu_drawer_item2 -> reloadFragment("현대홈쇼핑")
+                R.id.menu_drawer_item3 -> reloadFragment("NH홈쇼핑")
+            }
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
 
         //ranking list 세팅
         getRankingList()
@@ -58,6 +79,12 @@ class RankingFragment : Fragment() {
             RankingData(img = R.drawable.p_img_03, name = getString(R.string.name_3), price = getString(R.string.price_3))
         )
 
+    }
+
+    private fun reloadFragment(text: String) {
+
+        toolbar_title = toolbar.findViewById(R.id.toolbar_title)
+        toolbar_title.text = text
     }
 
 }
